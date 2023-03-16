4fc437f7831e30e80595722daf2f45961e5a28f1
Ariane-Sousa <ariane.crisousa@gmail.com>
2023-03-16 15:00:09 -0300

    Estrutura Spring Boot

---
+++ src/main/java/com/domrock/domrock/DomrockApplication.java
@@ -0,0 +1,13 @@
+package com.domrock.domrock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomrockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomrockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domrock/domrock/DomrockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domrock.domrock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomrockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

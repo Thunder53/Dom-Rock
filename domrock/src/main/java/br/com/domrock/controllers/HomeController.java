fba6d58506a00f37782a894f7652b2c6e13c0e86
Ariane-Sousa <ariane.crisousa@gmail.com>
2023-03-17 21:35:16 -0300

    Configuração do servidor Spring Boot

---
+++ domrock/src/main/java/br/com/domrock/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package br.com.domrock.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("home/index");
+		return mv;
+		
+	}
+
+}
---
+++ domrock/src/test/java/br/com/domrock/DomrockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.domrock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomrockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

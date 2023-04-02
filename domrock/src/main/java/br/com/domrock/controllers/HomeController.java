b6854bec132e2606b09750bba16c5131ccac0d75
Tobias Sousa <tfbsousa@hotmail.com>
2023-04-02 00:16:01 -0300

    Ajustes do Back-end da tela login

--- domrock/src/main/java/br/com/domrock/controllers/HomeController.java
+++ domrock/src/main/java/br/com/domrock/controllers/HomeController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.domrock.model.Administrador;
 import br.com.domrock.model.Usuario;
 import br.com.domrock.model.Vendedor;
+import br.com.domrock.model.DAOs.AdministradorDAO;
 import br.com.domrock.model.DAOs.UsuarioDAO;
 import br.com.domrock.model.DAOs.VendedorDAO;
 
@@ -18,36 +20,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class HomeController {
 	
 	
-	private UsuarioDAO usuarioDAO;
+	
 	
 	@GetMapping("/")
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("/home/Index.html");
+		mv.setViewName("/home/index.html");
 		return mv;
 
 	}
 	
-//	@GetMapping("/")
-//	public String getLogin (UsuarioDAO usuario) {
-//		usuario.buscaUsuario ("buscarUsuario");
-//		return "Login";
-//		
-//
-//	}
-//	
-//	@PostMapping("/login")
-//	public String login(@ModelAttribute UsuarioDAO usuarioDAO, Usuario usuario) {
-//		System.out.println("login request:" + usuarioDAO);
-//		UsuarioDAO buscaUsuario = usuarioDAO.buscaUsuario(usuario.getEmail(), usuario.getSenha());
-//		if(buscaUsuario != null) {
-//			usuario.addAttribute ("Login", buscaUsuario.getEmail());
-//			return "CRUD";
-//		}else {
-//			return "error_page";
-//		}
-//	}
-	
+	@GetMapping("./AdministradorDAO")
+	public String getLogin (AdministradorDAO administrador) {
+		administrador.buscaAdministrador ("buscaadministrador");
+		return "Login";
+		
+
+	}
 	
 	
 		

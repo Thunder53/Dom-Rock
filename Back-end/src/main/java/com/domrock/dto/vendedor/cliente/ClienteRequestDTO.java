de9fa4e0d9f0270b383ecc4cb52fdb644927d956
elizabethleite <101938881+elizabethleite@users.noreply.github.com>
2023-04-05 21:08:20 -0300

    Correção RTO do Cliente

--- Back-end/src/main/java/com/domrock/dto/vendedor/cliente/ClienteRequestDTO.java
+++ Back-end/src/main/java/com/domrock/dto/vendedor/cliente/ClienteRequestDTO.java
@@ -1,4 +1,4 @@
 package com.domrock.dto.vendedor.cliente;
 
-public record ClienteRequestDTO(String nome_cliente, String nome_gerencia, String produto_a, String produto_b, String produto_c) {
+public record ClienteRequestDTO(String nome_cliente, String nome_gerencia) {
 }

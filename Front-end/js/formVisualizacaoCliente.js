3561ddbb5aa38d3fa78dfe4aa2123a03a5c01ba6
Tobias Sousa <tfbsousa@hotmail.com>
2023-04-11 20:14:25 -0300

    Autalização do Diagrama de Classes

--- Front-end/js/formVisualizacaoCliente.js
+++ Front-end/js/formVisualizacaoCliente.js
@@ -1,19 +1,24 @@
-function visualizar()
-    fetch("http://localhost:8080/vendedor",        
-     (document).ready(function() {
-        ajax({
-          url: 'http://localhost:8080/cliente', // URL do backend que fornece as informações do cliente
-          type: 'GET', // Método HTTP usado para fazer a solicitação
-          dataType: 'json', // Tipo de dados esperados em resposta
-          success: function(data) {
-            // Atualize os elementos HTML com os dados recebidos
-            $('.info_clientes td:eq(0)').text(data.cod_cliente);
-            $('.info_clientes td:eq(1)').text(data.nome_cliente);
-            $('.info_clientes td:eq(2)').text(data.nome_gerencia);
-          },
-          error: function() {
-            alert('Erro ao buscar informações do cliente');
-          }
-        });
-      })
-    );
+
+
+const formulario = document.querySelector("form");
+const Icod_cliente = document.querySelector(".cod_cliente");
+const Inome_cliente = document.querySelector(".nome_cliente");
+const Inome_gerencia = document.querySelector(".nome_gerencia");
+function buscar(){
+    fetch("http://localhost:8080/clientes",
+    {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "GET",
+        success: function(data) {
+          // Atualize os elementos HTML com os dados recebidos
+          ('.info_clientes td:eq(0)').text(data.cod_cliente);
+          ('.info_clientes td:eq(1)').text(data.nome_cliente);
+          ('.info_clientes td:eq(2)').text(data.nome_gerencia);
+        },
+    })
+    .then(function(res) {console.log(res)})
+    .catch(function(res) {console.log(res)})
+};

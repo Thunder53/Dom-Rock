d6e183571e4827407afb2cbc7610d17788ef7985
Tobias Sousa <tfbsousa@hotmail.com>
2023-04-18 21:39:11 -0300

    Atualização dos Scipts do Banco

--- Front-end/js/formCliente.js
+++ Front-end/js/formCliente.js
@@ -1,9 +1,6 @@
 const formulario = document.querySelector("form");
 const Inome_cliente = document.querySelector(".nome_cliente");
 const Inome_gerencia = document.querySelector(".nome_gerencia");
-const Iproduto_a = document.querySelector(".produto_a");
-const Iproduto_b = document.querySelector(".produto_b");
-const Iproduto_c = document.querySelector(".produto_c");
 
 function cadastrar(){
     fetch("http://localhost:8080/cliente",
@@ -16,9 +13,6 @@ function cadastrar(){
         body: JSON.stringify({
             nome_cliente: Inome_cliente.value,
             nome_gerencia: Inome_gerencia.value,
-            produto_a: Iproduto_a.value,
-            produto_b: Iproduto_b.value,
-            produto_c: Iproduto_c.value
         })
     })
     .then(function(res) {console.log(res)})
@@ -28,15 +22,12 @@ function cadastrar(){
 function limpar(){
     Inome_cliente.value = "";
     Inome_gerencia.value = "";
-    Iproduto_a.value = "";
-    Iproduto_b.value = "";
-    Iproduto_c.value = "";
 }
 
 formulario.addEventListener('submit', function(event){
     event.preventDefault();
 
-    if (Inome_cliente.value === "" || Inome_cliente.value === "" || Iproduto_a.value === "" || Iproduto_b.value === "" || Iproduto_c.value === "") {
+    if (Inome_cliente.value === "" || Inome_cliente.value === "" ) {
         alert("Insira todos os campos.");
     } else {
         cadastrar();
--- Front-end/js/formVisualizacaoCliente.js
+++ Front-end/js/formVisualizacaoCliente.js
@@ -1,24 +1,44 @@
 
-
-const formulario = document.querySelector("form");
-const Icod_cliente = document.querySelector(".cod_cliente");
-const Inome_cliente = document.querySelector(".nome_cliente");
-const Inome_gerencia = document.querySelector(".nome_gerencia");
 function buscar(){
-    fetch("http://localhost:8080/clientes",
-    {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        method: "GET",
-        success: function(data) {
-          // Atualize os elementos HTML com os dados recebidos
-          ('.info_clientes td:eq(0)').text(data.cod_cliente);
-          ('.info_clientes td:eq(1)').text(data.nome_cliente);
-          ('.info_clientes td:eq(2)').text(data.nome_gerencia);
-        },
-    })
-    .then(function(res) {console.log(res)})
-    .catch(function(res) {console.log(res)})
+    const url = "http://localhost:8080/cliente";
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const boxview = document.querySelector(".boxview");
+        const infoCliente = document.querySelector(".info_cliente");
+
+        // Remove linhas anteriores da tabela, mantendo apenas a primeira linha de cabeçalho
+        while (infoCliente.rows.length > 1) {
+          infoCliente.deleteRow(1);
+        }
+
+        // Adiciona cada cliente como uma nova linha na tabela
+        data.forEach(clienteList => {
+          const row = infoCliente.insertRow();
+          row.insertCell().textContent = clienteList.cod_cliente;
+          row.insertCell().textContent = clienteList.nome_cliente;
+          row.insertCell().textContent = clienteList.nome_gerencia;
+          row.insertCell().textContent = clienteList.fk_usuario_id;
+        });
+
+        row.cells[0].classList.add("cod_cliente");
+        row.cells[1].classList.add("nome_cliente");
+        row.cells[2].classList.add("nome_gerencia");
+        row.cells[3].classList.add("fk_usuario_id");
+        
+
+        // Exibe a boxview
+       
+      })
+      .catch(error => console.error(error));
 };
+
+// Executa a função buscar assim que o documento HTML é completamente carregado
+document.addEventListener('DOMContentLoaded', buscar);
+
+
+const botao = document.getElementById("botao");
+botao.addEventListener("click", function() {
+    window.location.href = "formCarteiraCliente.html";
+});

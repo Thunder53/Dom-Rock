da53dc3d28fa133e4b52d44108430f3cc6a54ef0
Ariane-Sousa <ariane.crisousa@gmail.com>
2023-04-23 13:46:54 -0300

    Gráfico de vendas estimas e quando foram atualizadas

--- Front-end/js/dadosVendas.js
+++ Front-end/js/dadosVendas.js
@@ -1,23 +1,16 @@
-$.ajax({
-    url: 'http://localhost:8080/venda',
-    type: 'GET',
-    success: function(result) {
-        var labels = [];
-        var data = [];
-
-        for (var i = 0; i < result.length; i++) {
-            labels.push(result[i].atualizada_em);
-            data.push(result[i].quant_estimada);
-        }
-
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var chart = new Chart(ctx, {
+fetch('http://localhost:8080/venda')
+    .then(response => response.json())
+    .then(data => {
+        const quantidades = data.map(venda => venda.quant_estimada / 100000000000);
+        const labels = data.map(venda => venda.atualizada_em);
+        const ctx = document.getElementById('myChart').getContext('2d');
+        const chart = new Chart(ctx, {
             type: 'bar',
             data: {
                 labels: labels,
                 datasets: [{
                     label: 'Quantidade Estimada',
-                    data: data,
+                    data: quantidades,
                     backgroundColor: 'rgba(54, 162, 235, 0.2)',
                     borderColor: 'rgba(54, 162, 235, 1)',
                     borderWidth: 1
@@ -25,16 +18,11 @@ $.ajax({
             },
             options: {
                 scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:true
-                        }
-                    }]
+                    yAxes: [{}]
                 }
             }
         });
-    },
-    error: function(error) {
-        console.log(error);
-    }
-});
+    })
+    .catch(error => {
+        console.error(error);
+    });

e52e7a95ed85645a372b83e6c69503075073fbca
Ariane-Sousa <ariane.crisousa@gmail.com>
2023-04-23 14:29:10 -0300

    Atualização

--- Front-end/js/dadosVendas.js
+++ Front-end/js/dadosVendas.js
@@ -1,28 +1,41 @@
 fetch('http://localhost:8080/venda')
-    .then(response => response.json())
-    .then(data => {
-        const quantidades = data.map(venda => venda.quant_estimada / 100000000000);
-        const labels = data.map(venda => venda.atualizada_em);
-        const ctx = document.getElementById('myChart').getContext('2d');
-        const chart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'Quantidade Estimada',
-                    data: quantidades,
-                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                    borderColor: 'rgba(54, 162, 235, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{}]
-                }
+  .then(response => response.json())
+  .then(data => {
+    const quantidadesEstimadas = data.map(venda => venda.quant_estimada / 100000000000);
+    const quantidadesVendidas = data.map(venda => venda.quant_vendida / 100000000000);
+    const labels = data.map(venda => venda.atualizada_em);
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const chart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Quantidade Estimada',
+          data: quantidadesEstimadas,
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1
+        }, {
+          label: 'Quantidade Vendida',
+          data: quantidadesVendidas,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              callback: function (value, index, ticks) {
+                return (value / 1000).toFixed(0) + 'K';
+              }
             }
-        });
-    })
-    .catch(error => {
-        console.error(error);
+          }]
+        }
+      }
     });
+  })
+  .catch(error => {
+    console.error(error);
+  });

4b7ad999c8c2e7458f66b4f838451f14e65a8240
Tobias Sousa <tfbsousa@hotmail.com>
2023-05-09 22:07:17 -0300

    Atualização da tela de home

--- Front-end/js/homeAdmin.js
+++ Front-end/js/homeAdmin.js
@@ -1,6 +1,6 @@
-const botao_cliente = document.getElementById("cliente");
+const botao_cliente = document.getElementById("#botao");
 botao_cliente.addEventListener("click", function() {
-  window.location.href = "formVisualizacaoCliente.html";
+  window.location.href = "../view/formVisualizacaoCliente.html";
 });
 
 const botao_dados    = document.getElementById("dados");
@@ -12,3 +12,4 @@ const botao_vendedor = document.getElementById("vendedor");
 botao_vendedor.addEventListener("click", function() {
   window.location.href = "formVendedor.html";
 });
+

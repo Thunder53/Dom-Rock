2b29dc7a368f5fdbc54b699f37b7de82ea3a7e6c
Ariane Sousa <ariane.crisousa@gmail.com>
2023-05-13 19:31:25 -0300

    Cadastro de produtos

--- Front-end/javascript/formProduto.js
+++ Front-end/javascript/formProduto.js
@@ -1,40 +1,18 @@
 const formulario = document.querySelector("form");
 const Inome_produto = document.getElementById("nome_produto");
-const Inome_cliente = document.getElementById("nome_cliente");
-const select = document.getElementById('caixa-de-selecao');
-let list = [];
-
 
 function cadastrar() {
-    const nomeVendedor = select.value.toString();
-    fetch(`http://localhost:8080/usuario/usuario-por-nome?nome=${nomeVendedor}`)
-      .then(response => response.json())
-      .then(data => {
-        const idVendedor = parseInt(data);
-        console.log(idVendedor);
-        list.push(idVendedor); 
-        const primeiroValor = list[0];
-        const id = parseInt(primeiroValor);
-        console.log(id);
-        fetch("http://localhost:8080/cliente", {
+        fetch("http://localhost:8080/produto", {
           headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
           },
           method: "POST",
           body: JSON.stringify({
-            nome_cliente: Inome_cliente.value,
-            nome_gerencia: Inome_gerencia.value,
-            fk_usuario_id: id
+            nome_produto: Inome_produto.value,
           })
         })
-      })
-      .catch(error => {
-        console.error(error);
-        alert("Erro ao buscar vendedor.");
-      });
   };
-
 
 function limpar(){
     Inome_cliente.value = "";
@@ -43,23 +21,10 @@ function limpar(){
 
 formulario.addEventListener('submit', function(event){
     event.preventDefault();
-
-    if (Inome_cliente.value === "" || Inome_cliente.value === "" ) {
+    if (Inome_produto.value === "") {
         alert("Insira todos os campos.");
     } else {
         cadastrar();
-        alert("Cliente cadastrado com sucesso.");
+        alert("Produto cadastrado com sucesso.");
     }
 });
-
-
-fetch("http://localhost:8080/usuario/vendedores")
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(usuario => {
-            const option = document.createElement('option');
-            option.text = usuario.nome;
-            select.appendChild(option);
-        });
-    })
-    .catch(error => console.error(error));
--- Front-end/javascript/formVisualizacaoCliente.js
+++ Front-end/javascript/formVisualizacaoCliente.js
@@ -7,12 +7,10 @@ function buscar() {
       const boxview = document.querySelector(".boxview");
       const infoCliente = document.querySelector(".info_cliente");
 
-      // Remove linhas anteriores da tabela, mantendo apenas a primeira linha de cabeçalho
       while (infoCliente.rows.length > 1) {
         infoCliente.deleteRow(1);
       }
 
-      // Adiciona cada cliente como uma nova linha na tabela
       data.forEach(cliente => {
         const row = infoCliente.insertRow();
         row.insertCell().textContent = cliente.Nome_Cliente;
@@ -24,17 +22,11 @@ function buscar() {
         row.cells[2].classList.add("nome");
        });
 
-      // Exibe a boxview
     })
     .catch(error => console.error(error));
 };
 
-// Executa a função buscar
-
-
-// Executa a função buscar assim que o documento HTML é completamente carregado
 document.addEventListener('DOMContentLoaded', buscar);
-
 
 const botao = document.getElementById("botao");
 botao.addEventListener("click", function() {

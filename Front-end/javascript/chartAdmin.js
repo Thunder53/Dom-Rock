d2ebaf3b7a447188a3b7d87a319574ca19a21063
Ariane Sousa <ariane.crisousa@gmail.com>
2023-05-14 14:14:18 -0300

    Filtro data

--- Front-end/javascript/chartAdmin.js
+++ Front-end/javascript/chartAdmin.js
@@ -28,3 +28,35 @@ fetch('http://localhost:8080/venda/topVendedores')
   .catch(function(error) {
     console.log(error);
   });
+
+  fetch('http://localhost:8080/venda')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    var dados = data.map(function(item) {
+      return { y: item.criada_em, a: item.quant_estimada, b: item.quant_vendida};
+    });
+
+    var config = {
+      data: dados,
+      xkey: 'y',
+      ykeys: ['a', 'b'],
+      labels: ['Total Income', 'Total Outcome'],
+      fillOpacity: 0.6,
+      hideHover: 'auto',
+      behaveLikeLine: true,
+      resize: true,
+      pointFillColors:['#ffffff'],
+      pointStrokeColors: ['black'],
+      lineColors:['blue']
+    };
+
+    config.element = 'bar-chart';
+    config.Bar = true;
+    Morris.Bar(config);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+

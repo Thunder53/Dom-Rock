74f2b107528432df5b162b8880eea3e6d8deefe7
beatricelopes <beatricelopes@outlook.com>
2023-05-28 15:23:07 -0300

    changes

--- Front-end/javascript/chart-acima-meta.js
+++ Front-end/javascript/chart-acima-meta.js
@@ -3,27 +3,45 @@ fetch("http://localhost:8080/venda/acima-meta")
     return response.json();
   })
   .then(function (data) {
-    var dados = data.map(function (item) {
-      return { y: item.quant_vendida, a: item.fk_usuario_id };
+    fetch("http://localhost:8080/usuario/vendedores")
+    .then(function (response){
+      return response.json();
+    })
+    .then(function(vendedores){
+
+      var linha_tabela = [];
+
+      for (var lista = 0; lista<data.length; lista++){ 
+        for (var id = 0; id<vendedores.length; id++){ 
+          if (data[lista].fk_usuario_id == vendedores[id].id){
+            linha_tabela.push({y: vendedores[id].nome, a: data[lista].quant_vendida})
+          }
+        }
+      }
+
+      
+      var dados = linha_tabela 
+  
+      var config = {
+        data: dados,
+        xkey: "y",
+        ykeys: "a",
+        labels: ["Total"],
+        fillOpacity: 0.6,
+        hideHover: "auto",
+        behaveLikeLine: false,
+        pointSize: 100,
+        resize: true,
+        pointFillColors: ["#ffffff"],
+        pointStrokeColors: ["black"],
+        lineColors: ["blue"],
+      };
+  
+      config.element = "vendedoresAcimaMeta";
+      config.stacked = false;
+      Morris.Bar(config);
+    
     });
-
-    var config = {
-      data: dados,
-      xkey: "y",
-      ykeys: "a",
-      labels: ["Total"],
-      fillOpacity: 0.6,
-      hideHover: "auto",
-      behaveLikeLine: true,
-      resize: true,
-      pointFillColors: ["#ffffff"],
-      pointStrokeColors: ["black"],
-      lineColors: ["blue"],
-    };
-
-    config.element = "stacked";
-    config.stacked = true;
-    Morris.Bar(config);
   })
   .catch(function (error) {
     console.log(error);

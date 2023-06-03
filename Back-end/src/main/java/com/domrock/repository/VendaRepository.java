d05d34e6361d2a782981a532260d51d904c6faf0
Tobias Sousa <tfbsousa@hotmail.com>
2023-06-03 11:00:58 -0300

    Merging

--- Back-end/src/main/java/com/domrock/repository/VendaRepository.java
+++ Back-end/src/main/java/com/domrock/repository/VendaRepository.java
@@ -17,9 +17,6 @@ public interface VendaRepository extends JpaRepository<Venda, Long> {
 
     @Query("SELECT v,u FROM venda v, usuario u WHERE v.fk_usuario_id = u.id and v.quant_vendida > v.quant_estimada")
     List<Venda> findVendedoresAcimaMeta();
-    
-    @Query("SELECT v FROM venda v WHERE v.fk_usuario_id = ?1")
-    List<Venda> findByUsuario(Long fk_usuario_id);
 
     @Query("SELECT v FROM venda v WHERE v.fk_produto_cod_produto = ?1")
     List<Venda> findByProduto(Long id);
